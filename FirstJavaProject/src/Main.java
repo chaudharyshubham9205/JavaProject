be852e6c59d32fd1a7cdad0c1656b65f308e27ba
SHUBHAM CHAUDHARY <110493359+chaudharyshubham9205@users.noreply.github.com>
2022-08-20 02:00:53 +0530

    Delete FirstJavaProject/src directory

--- FirstJavaProject/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-	public static void main(String[] args){
-	    Square sq = new Square();  
-	    sq.print_shape();    // Object of Square class calling function of Shape class
-	    sq.print_rect();    // Object of Square class calling function of Rectangle class
-	  }
-}
